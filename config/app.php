efbbfded63e5e2030550a7d4e881ce3ee5a92ede
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-12-04 15:55:59 +0800

    Revert "feat: heroku integration"
    This reverts commit b322998f909ccf83bd087eb464040769ffd93a33.

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', env('HEROKU_APP_NAME') ? 'https://' . env('HEROKU_APP_NAME') . '.herokuapp.com' : 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => strpos(env('APP_KEY'), 'base64:') !== false ? env('APP_KEY') : substr(env('APP_KEY'), 0, 32),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
