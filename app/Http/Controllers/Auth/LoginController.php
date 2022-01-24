40c9012b379fb1c467c4cc9ad69da3bf671355b0
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-01-24 15:25:33 +0800

    Revert "feat: fixed alert response, both success and failed login"
    This reverts commit 63050972113b8d2eeb7cd71846d51eacb6fdf796.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,13 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use RealRashid\SweetAlert\Facades\Alert;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Http\JsonResponse;
-
-
-
 
 class LoginController extends Controller
 {
@@ -33,7 +26,6 @@ class LoginController extends Controller
      *
      * @var string
      */
-
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
@@ -44,48 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    protected function sendLoginResponse(Request $request)
-    {
-        $request->session()->regenerate();
-
-        $this->clearLoginAttempts($request);
-
-        Alert::success('', "You've Successfully Login!");
-
-
-        if ($response = $this->authenticated($request, $this->guard()->user())) {
-            return $response;
-        }
-
-        return $request->wantsJson()
-            ? new JsonResponse([], 204)
-            : redirect()->intended($this->redirectPath());
-    }
-
-    protected function sendFailedLoginResponse(Request $request)
-    {
-        Alert::error('', 'Login Failed');
-        throw ValidationException::withMessages([
-            $this->username() => [trans('auth.failed')],
-        ]);
-    }
-
-    public function logout(Request $request)
-    {
-        $this->guard()->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        if ($response = $this->loggedOut($request)) {
-            return $response;
-        }
-
-        return $request->wantsJson()
-            ? new JsonResponse([], 204)
-            : redirect('/login');
     }
 }
