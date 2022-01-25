81d03ada65701669d484d5bc7d59ca126d2b97f3
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-01-25 18:50:03 +0800

    feat(api): update routing + add authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\HeatmapDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public Routes
 
-Route::resource('/heatmap', HeatmapDataController::class);
+Route::post('/auth/register', [ApiAuthController::class, 'register']);
+Route::post('/auth/login', [ApiAuthController::class, 'login']);
+
+Route::get('/heatmap', [HeatmapDataController::class, 'index']);
+
+
+// Protected Routes
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/auth/logout', [ApiAuthController::class, 'logout']);
+
+    Route::post('/heatmap', [HeatmapDataController::class, 'store']);
+    Route::delete('/heatmap', [HeatmapDataController::class, 'destroy']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
