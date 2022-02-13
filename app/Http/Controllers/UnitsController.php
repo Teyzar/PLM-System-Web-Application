c7bf53dd501d63e88c262095d2de619ae0500722
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-02-13 23:36:44 +0800

    feat(UnitsController): implement mqtt connection

--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -6,6 +6,9 @@ use App\Events\HeatmapUpdate;
 use App\Models\Unit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use PhpMqtt\Client\Exceptions\MqttClientException;
+use PhpMqtt\Client\Facades\MQTT;
+use PhpMqtt\Client\MqttClient;
 
 class UnitsController extends Controller
 {
@@ -23,24 +26,62 @@ class UnitsController extends Controller
 
     public function store(Request $request)
     {
-
         $rules = [
             'phone_number' => 'required|unique:units',
         ];
+
         $validation = Validator::make($request->all(), $rules);
 
         if ($validation->fails()) {
             return back()->withErrors($validation)->withInput();
-        } else {
-            Unit::create([
-                'active' => false,
-                'phone_number' => $request->phone_number
-            ]);
-
-            toast('Unit Succesfully Registered!', 'success');
-
-            return redirect()->back();
         }
+
+        $phone_number = $request->phone_number;
+
+        $success = true;
+
+        try {
+            $mqtt = MQTT::connection();
+            $mqtt->subscribe('plms-clz/units/response', function (string $topic, string $message) use ($mqtt, $phone_number) {
+                $data = json_decode($message);
+
+                if ($phone_number != $data->phone_number) return;
+
+                Unit::create([
+                    'active' => $data->active,
+                    'latitude' => $data->latitude,
+                    'longitude' => $data->longitude,
+                    'phone_number' => $data->phone_number
+                ]);
+
+                $mqtt->interrupt();
+            });
+
+            $mqtt->registerLoopEventHandler(
+                function (MqttClient $client, float $elapsedTime) use (&$success) {
+                    if ($elapsedTime > 30) {
+                        $success = false;
+                        $client->interrupt();
+                    }
+                }
+            );
+
+            $mqtt->publish('plms-clz/units/register', $phone_number);
+
+            $mqtt->loop();
+
+            $mqtt->disconnect();
+        } catch (MqttClientException $error) {
+            $success = false;
+        }
+
+        if ($success) {
+            toast('Unit Succesfully Registered!', 'success');
+        } else {
+            toast('Unit Failed to Respond!', 'error');
+        }
+
+        return redirect()->back();
     }
 
     public function update(Request $request, String $phone_number)
