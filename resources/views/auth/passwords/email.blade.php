48dbb0a78af6cab67dcff18a579953d03daa308a
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-02-26 12:06:04 +0800

    feat: update forgotpassword page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,63 @@
 @extends('layouts.app')
 
+@section('head')
+
+@endsection
+
 @section('content')
-<div class="container py-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <div class="container col-md-8" style="margin-top: 10%">
+        <div class="container">
+            @if (session('status'))
+                <div class="alert alert-success justify-content-center d-flex text-start border-0" role="alert"
+                    style="margin-left: 30%; margin-right: 30% ">
+                    <i class="fa-solid fa-inbox-out fs-5 pe-3 text-secondary"></i>{{ session('status') }}
+                </div>
+            @endif
+        </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div id="modal-content" class="modal-content w-100 border border-secondary bg-light">
+                <div class="modal-header border-0">
+                    <h5 class="fs-6 d-flex text-secondary">Reset Password</h5>
+                </div>
+                <div class="modal-body border-0">
+                    <div class="panel-body">
+                        <div class="text-center">
+                            <h3><i class="fa fa-lock fa-3x text-danger"></i></h3>
+                            <h4 class="text-center text-dark">Forgot password?</h4>
+                            <div class="panel-body">
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+                                    <div class="form-group pt-3 justify-content-center">
+                                        <div class="input-group">
+                                            <i
+                                                class="bi bi-envelope-check fs-2 pe-sm-2 text-dark opacity-75 align-items-center"></i>&nbsp;
+                                            <input id="email" type="email"
+                                                class="form-control float-start border @error('email') is-invalid @enderror"
+                                                name="email" required autocomplete="email" autofocus
+                                                placeholder="Enter E-mail Address here.." value="{{ old('email') }}">
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <span id="errormsg"></span>
+                                    </div>
+                                    <div class="form-group pt-2">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Send Password Reset Link') }}
+                                        </button>
+                                    </div>
+                                    <input type="hidden" class="hide" name="token" id="token" value="">
+                                </form>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="footer d-flex justify-content-center opacity-75">
+            Copyright &copy; 2022 &mdash; Power Line Monitoring System
+        </div>
     </div>
-</div>
 @endsection
