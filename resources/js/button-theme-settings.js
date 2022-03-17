629f91a604844e1b5ccfd96e4928cd957ed379d7
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-03-17 18:12:36 +0800

    chore(button-theme) : update theme settings

--- resources/js/button-theme-settings.js
+++ resources/js/button-theme-settings.js
@@ -32,34 +32,108 @@ lightModeButton.addEventListener('click', () => {
     }
 });
 
-const resetBtn = document.querySelector('#resetBtn');
+
+//condensed Button
+
+const condensedBtn = document.querySelector('#condensed-check');
+const defaultBtn = document.querySelector('#default-size-check');
+
+let condensed = localStorage.getItem('condensed');
+
+const enableCondensed = () => {
+    if (darkMode == "enabled" && boxWidth == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "condensed"}, "width": "boxed"}');
+    } else {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "condensed"}, "width": "fluid"}');
+    }
+    if (darkMode == "enabled" && boxWidth == "disabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "condensed"}, "width": "fluid"}');
+    }
+    if (darkMode == "disabled" && boxWidth == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "condensed"}, "width": "boxed"}');
+    }
+    if (darkMode == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "condensed"}, "width": "fluid"}');
+    }
+    localStorage.setItem('condensed', 'enabled');
+}
+
+const enableDefault = () => {
+    if (darkMode == "enabled" && boxWidth == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "default"}, "width": "boxed"}');
+    } else {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "default"}, "width": "fluid"}');
+    }
+    if (darkMode == "enabled" && boxWidth == "disabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "default"}, "width": "fluid"}');
+    }
+    if (darkMode == "disabled" && boxWidth == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "default"}, "width": "boxed"}');
+    }
+    if (darkMode == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "default"}, "width": "fluid"}');
+    }
+    localStorage.setItem('condensed', 'disabled');
+}
+
+if (condensed == "enabled") {
+    enableCondensed();
+}
+condensedBtn.addEventListener('click', () => {
+    condensed = localStorage.getItem('condensed');
+
+    if (condensed !== "enabled") {
+        enableCondensed();
+    }
+})
+defaultBtn.addEventListener('click', () => {
+    condensed = localStorage.getItem('condensed');
+    if (condensed == "enabled") {
+        enableDefault();
+    }
+});
+
+
+//width size layout
 let boxWidth = localStorage.getItem("boxedWidth")
 
 const boxedWidthBtn = document.querySelector('#boxed-check');
 const fluidWidthBtn = document.querySelector('#fluid-check');
 
 
-
-
 const enableBoxWidth = () => {
+    if (condensed == "enabled" && darkMode =="disabled") {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "default" }, "width": "boxed"}');
+    }
     if (darkMode == "enabled") {
         body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark"}, "width": "boxed"}');
-    } else {
-        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light"}, "width": "boxed"}');
+    } else if (darkMode == "enabled" && condensed == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "condensed" }, "width": "boxed"}');
+    } else if (darkMode == "disabled" && condensed == "enabled"){
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "condensed"}, "width": "boxed"}');
+    } else if (darkMode == "enabled" && condensed == "disabled"){
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "default"}, "width": "boxed"}');
     }
     localStorage.setItem('boxedWidth', "enabled");
 }
 
 const enableFluidWidth = () => {
+    if (condensed == "enabled" && darkMode =="disabled") {
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "default" }, "width": "fluid"}');
+    }
     if (darkMode == "enabled") {
         body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark"}, "width": "fluid"}');
-    } else {
-        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light"}, "width": "fluid"}');
+    } else if (darkMode == "enabled" && condensed == "enabled") {
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "condensed" }, "width": "fluid"}');
+    } else if (darkMode == "disabled" && condensed == "enabled"){
+        body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light", "size" : "condensed"}, "width": "fluid"}');
+    } else if (darkMode == "enabled" && condensed == "disabled"){
+        body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark", "size" : "default"}, "width": "fluid"}');
     }
     localStorage.setItem('boxedWidth', "disabled");
 }
 
-if (boxWidth === "enabled") {
+if (boxWidth == "enabled") {
     enableBoxWidth();
 }
 
@@ -77,12 +151,18 @@ fluidWidthBtn.addEventListener('click', () => {
 });
 
 
-resetBtn.addEventListener('click', () => {
-    darkMode = localStorage.getItem("darkMode");
-    if (darkMode == "enabled") {
-        enableLightMode();
-    }
-});
+// const resetBtn = document.querySelector('#resetBtn');
+
+// resetBtn.addEventListener('click', () => {
+
+//     body.setAttribute('data-layout', '{"mode": "light", "sidebar" : {"color" : "light", size : "default"}, "width" : "fluid"}');
+//     localStorage.setItem('boxedWidth', "disabled");
+//     localStorage.setItem('darkMode', "disabled");
+//     localStorage.setItem('condensed', "disabled");
+// });
+
+
+
 
 
 
