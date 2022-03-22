971e5eb8ec281dc10617ffb32aa17de9109f81df
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-03-23 01:44:33 +0800

    chore(app): remove admin image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,8 +59,8 @@
                             <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light"
                                 data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
                                 aria-expanded="false">
-                                <img src="{{ mix('images/user-3.jpg') }}" alt="user-image" class="rounded-circle">
-                                <span class="pro-user-name ms-1">
+                                {{-- <img src="{{ mix('images/user-3.jpg') }}" alt="user-image" class="rounded-circle"> --}}
+                                <span class="ms-1">
                                     {{ ucwords(Auth::user()->name) }}<i class="mdi mdi-chevron-down"></i>
                                 </span>
                             </a>
