6c742c5cf0cf3cfd8a3ac28409c0047e09bcfb07
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-03-23 09:17:01 +0800

    Revert "test(home.blade): heatmapData not iterable"
    This reverts commit 74ec536b35d48df6136b4d108e14b3e13c0dcf86.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,6 @@
 @section('head')
     <script>
         let map, heatmap, heatmapData;
-        const _heatmapData = {!! $heatmapData !!};
-
-        console.log(_heatmapData);
 
         function initMap() {
             map = new google.maps.Map(document.getElementById("map"), {
@@ -37,8 +34,7 @@
 
             // Link heatmap with map
             heatmap.setMap(map);
-
-            for (const data of [..._heatmapData]) {
+            for (const data of {!! $heatmapData !!}) {
                 heatmapData.push({
                     id: data.id,
                     weight: 1,
