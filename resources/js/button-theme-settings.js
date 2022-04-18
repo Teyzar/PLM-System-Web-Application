60ddde23dcd0a6d742bbe97a27f70151b6704aeb
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-18 21:54:47 +0800

    refactor: map themes on button theme settings

--- resources/js/button-theme-settings.js
+++ resources/js/button-theme-settings.js
@@ -2,16 +2,147 @@ let darkMode = localStorage.getItem('darkMode');
 const darkModeButton = document.querySelector('#dark-mode-check');
 const lightModeButton = document.querySelector('#light-mode-check');
 
+const mapDark = [
+    {
+        elementType: "geometry",
+        stylers: [{
+            color: "#242f3e"
+        }]
+    },
+    {
+        elementType: "labels.text.stroke",
+        stylers: [{
+            color: "#242f3e"
+        }]
+    },
+    {
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#746855"
+        }]
+    },
+    {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#d59563"
+        }],
+    },
+    {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#d59563"
+        }],
+    },
+    {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{
+            color: "#263c3f"
+        }],
+    },
+    {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#6b9a76"
+        }],
+    },
+    {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{
+            color: "#38414e"
+        }],
+    },
+    {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{
+            color: "#212a37"
+        }],
+    },
+    {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#9ca5b3"
+        }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{
+            color: "#746855"
+        }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{
+            color: "#1f2835"
+        }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#f3d19c"
+        }],
+    },
+    {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{
+            color: "#2f3948"
+        }],
+    },
+    {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#d59563"
+        }],
+    },
+    {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{
+            color: "#17263c"
+        }],
+    },
+    {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{
+            color: "#515c6d"
+        }],
+    },
+    {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{
+            color: "#17263c"
+        }],
+    },
+];
+
+const mapLight = [];
+
 const body = document.querySelector('body');
 
 const enableDarkmode = () => {
     body.setAttribute('data-layout', '{"mode" : "dark", "sidebar" : {"color" : "dark"}}');
+    if (typeof map !== "undefined") map.setOptions({ styles: mapDark });
 
     localStorage.setItem('darkMode', "enabled");
 }
 
 const enableLightMode = () => {
     body.setAttribute('data-layout', '{"mode" : "light", "sidebar" : {"color" : "light"}}');
+    if (typeof map !== "undefined") map.setOptions({ styles: mapLight });
+
     localStorage.setItem('darkMode', "disabled");
 }
 
--- resources/views/incidents.blade.php
+++ resources/views/incidents.blade.php
@@ -2,7 +2,6 @@
 
 
 @section('head')
-
     <script src="{{ asset('libs/tippy.js/tippy.all.min.js') }}"></script>
     <script>
         let map, markers, bounds, infoWindow;
@@ -16,156 +15,11 @@
             map = new google.maps.Map(document.getElementById("map"), {
                 zoom: 15,
                 center: cadiz,
-                mapTypeId: "roadmap"
+                mapTypeId: "roadmap",
+                styles: darkMode === "enabled" ? mapDark : mapLight,
             });
 
             markers = [];
-
-            const darkMode = localStorage.getItem('darkMode');
-            const darkModeButton = document.querySelector('#dark-mode-check');
-            const lightModeButton = document.querySelector('#light-mode-check');
-
-            const enableDarkMode = () => {
-                map.setOptions({
-                    styles: [{
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#242f3e"
-                            }]
-                        },
-                        {
-                            elementType: "labels.text.stroke",
-                            stylers: [{
-                                color: "#242f3e"
-                            }]
-                        },
-                        {
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#746855"
-                            }]
-                        },
-                        {
-                            featureType: "administrative.locality",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#d59563"
-                            }],
-                        },
-                        {
-                            featureType: "poi",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#d59563"
-                            }],
-                        },
-                        {
-                            featureType: "poi.park",
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#263c3f"
-                            }],
-                        },
-                        {
-                            featureType: "poi.park",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#6b9a76"
-                            }],
-                        },
-                        {
-                            featureType: "road",
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#38414e"
-                            }],
-                        },
-                        {
-                            featureType: "road",
-                            elementType: "geometry.stroke",
-                            stylers: [{
-                                color: "#212a37"
-                            }],
-                        },
-                        {
-                            featureType: "road",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#9ca5b3"
-                            }],
-                        },
-                        {
-                            featureType: "road.highway",
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#746855"
-                            }],
-                        },
-                        {
-                            featureType: "road.highway",
-                            elementType: "geometry.stroke",
-                            stylers: [{
-                                color: "#1f2835"
-                            }],
-                        },
-                        {
-                            featureType: "road.highway",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#f3d19c"
-                            }],
-                        },
-                        {
-                            featureType: "transit",
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#2f3948"
-                            }],
-                        },
-                        {
-                            featureType: "transit.station",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#d59563"
-                            }],
-                        },
-                        {
-                            featureType: "water",
-                            elementType: "geometry",
-                            stylers: [{
-                                color: "#17263c"
-                            }],
-                        },
-                        {
-                            featureType: "water",
-                            elementType: "labels.text.fill",
-                            stylers: [{
-                                color: "#515c6d"
-                            }],
-                        },
-                        {
-                            featureType: "water",
-                            elementType: "labels.text.stroke",
-                            stylers: [{
-                                color: "#17263c"
-                            }],
-                        },
-                    ]
-                });
-            };
-
-            const enableLightMode = () => {
-                map.setOptions({
-                    styles: []
-                });
-            }
-
-            if (darkMode === "enabled") {
-                enableDarkMode();
-            }
-
-            darkModeButton.addEventListener('click', enableDarkMode);
-            lightModeButton.addEventListener('click', enableLightMode);
         }
 
         function updateMarkers(units) {
